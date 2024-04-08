324d6df38d6475cee5062fd18d9ccdd98b0eb6ae
Kyumin <kyumin@Kyuminui-noteubug.local>
2024-04-08 23:36:47 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/meritz/MeritzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meritz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeritzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
