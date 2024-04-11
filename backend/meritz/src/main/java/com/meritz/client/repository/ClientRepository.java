2ef32ad145468311080b7994516f6340d3b896d3
kkyu-min <lkm454545@naver.com>
2024-04-11 10:21:50 +0900

    feat: global Entity

--- backend/meritz/src/main/java/com/meritz/client/repository/ClientRepository.java
+++ backend/meritz/src/main/java/com/meritz/client/repository/ClientRepository.java
@@ -1,7 +1,7 @@
 package com.meritz.client.repository;
 
+import com.meritz.client.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface ClientRepository extends JpaRepository {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
