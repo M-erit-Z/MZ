6aa7ec57728e8d3d7f0d6f8528111bf2a7b739e4
kkyu-min <lkm454545@naver.com>
2024-04-11 10:49:12 +0900

    branch 설정

--- backend/meritz/src/main/java/com/meritz/client/entity/Client.java
+++ backend/meritz/src/main/java/com/meritz/client/entity/Client.java
@@ -2,7 +2,6 @@ package com.meritz.client.entity;
 
 import com.meritz.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
