2cadf35b7153eaa64f2dcabd94d2a95e88a30e77
k-minsik <smin58810@gmail.com>
2024-04-12 12:51:21 +0900

    fix: Fix offer, icecandidate, answer resp

--- backend/meritz/src/main/java/com/meritz/websocket/SignalingController.java
+++ backend/meritz/src/main/java/com/meritz/websocket/SignalingController.java
@@ -35,46 +35,46 @@ public class SignalingController {
         return candidate;
     }
 
-//    @MessageMapping("/peer/offer/{camKey}/{roomId}")
-//    @SendTo("/topic/peer/offer/{camKey}/{roomId}")
-//    public String PeerHandleOffer(@Payload String offer,
-//                                  @DestinationVariable(value = "camKey") String camKey,
-//                                  @DestinationVariable(value = "roomId") String roomId) {
-//        log.info("[OFFER] {} : {}", camKey, offer);
-//        return offer;
-//    }
-//
-//    @MessageMapping("/peer/iceCandidate/{camKey}/{roomId}")
-//    @SendTo("/topic/peer/iceCandidate/{camKey}/{roomId}")
-//    public String PeerHandleIceCandidate(@Payload String candidate,
-//                                         @DestinationVariable(value = "camKey") String camKey,
-//                                         @DestinationVariable(value = "roomId") String roomId) {
-//        log.info("[ICECANDIDATE] {} : {}", camKey, candidate);
-//        return candidate;
-//    }
-//
-//    @MessageMapping("/peer/answer/{camKey}/{roomId}")
-//    @SendTo("/topic/peer/answer/{camKey}/{roomId}")
-//    public String PeerHandleAnswer(@Payload String answer, @DestinationVariable(value = "roomId") String roomId,
-//                                   @DestinationVariable(value = "camKey") String camKey) {
-//        log.info("[ANSWER] {} : {}", camKey, answer);
-//        return answer;
-//    }
-//
-//    //camKey 를 받기위해 신호를 보내는 webSocket
-//    @MessageMapping("/call/key")
-//    @SendTo("/topic/call/key")
-//    public String callKey(@Payload String message) {
-//        log.info("[Key] : {}", message);
-//        return message;
-//    }
-//
-//    //자신의 camKey 를 모든 연결된 세션에 보내는 webSocket
-//    @MessageMapping("/send/key")
-//    @SendTo("/topic/send/key")
-//    public String sendKey(@Payload String message) {
-//        return message;
-//    }
+    @MessageMapping("/peer/offer/{camKey}/{roomId}")
+    @SendTo("/topic/peer/offer/{camKey}/{roomId}")
+    public String PeerHandleOffer(@Payload String offer,
+                                  @DestinationVariable(value = "camKey") String camKey,
+                                  @DestinationVariable(value = "roomId") String roomId) {
+        log.info("[OFFER] {} : {}", camKey, offer);
+        return offer;
+    }
+
+    @MessageMapping("/peer/iceCandidate/{camKey}/{roomId}")
+    @SendTo("/topic/peer/iceCandidate/{camKey}/{roomId}")
+    public String PeerHandleIceCandidate(@Payload String candidate,
+                                         @DestinationVariable(value = "camKey") String camKey,
+                                         @DestinationVariable(value = "roomId") String roomId) {
+        log.info("[ICECANDIDATE] {} : {}", camKey, candidate);
+        return candidate;
+    }
+
+    @MessageMapping("/peer/answer/{camKey}/{roomId}")
+    @SendTo("/topic/peer/answer/{camKey}/{roomId}")
+    public String PeerHandleAnswer(@Payload String answer, @DestinationVariable(value = "roomId") String roomId,
+                                   @DestinationVariable(value = "camKey") String camKey) {
+        log.info("[ANSWER] {} : {}", camKey, answer);
+        return answer;
+    }
+
+    //camKey 를 받기위해 신호를 보내는 webSocket
+    @MessageMapping("/call/key")
+    @SendTo("/topic/call/key")
+    public String callKey(@Payload String message) {
+        log.info("[Key] : {}", message);
+        return message;
+    }
+
+    //자신의 camKey 를 모든 연결된 세션에 보내는 webSocket
+    @MessageMapping("/send/key")
+    @SendTo("/topic/send/key")
+    public String sendKey(@Payload String message) {
+        return message;
+    }
 
 
 }
