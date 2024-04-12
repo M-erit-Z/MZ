615a009547b2775c78da2e026d1aef6c00bb00a5
k-minsik <smin58810@gmail.com>
2024-04-12 12:53:15 +0900

    fix: Add EnableJpaAuditing annotation

--- backend/meritz/src/main/java/com/meritz/MeritzApplication.java
+++ backend/meritz/src/main/java/com/meritz/MeritzApplication.java
@@ -2,8 +2,10 @@ package com.meritz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MeritzApplication {
 
     public static void main(String[] args) {
