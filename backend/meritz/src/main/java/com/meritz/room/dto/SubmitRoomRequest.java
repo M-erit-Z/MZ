9a644baeb87dc6c15992ad211da30dd722e7f6ae
k-minsik <smin58810@gmail.com>
2024-04-15 16:41:56 +0900

    feat: 사고접수 완료(담당자)

---
+++ backend/meritz/src/main/java/com/meritz/room/dto/SubmitRoomRequest.java
@@ -0,0 +1,18 @@
+package com.meritz.room.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class SubmitRoomRequest {
+    private Long roomId;
+    private String clientName;
+    private String clientPhone;
+    private LocalDateTime occurTime;
+    private String location;
+    private String content;
+    private String chatting;
+}
