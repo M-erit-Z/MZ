279fed7b7a667cde1889c6071e562154c870339a
k-minsik <smin58810@gmail.com>
2024-04-15 15:38:27 +0900

    feat: 메인 페이지

---
+++ backend/meritz/src/main/resources/static/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById('clientEntry').addEventListener('click', function() {
+    window.location.href = '/register.html'; // 고객 버튼 클릭 시 '/room'으로 리다이렉트
+});
+
+document.getElementById('managerEntry').addEventListener('click', function() {
+    window.location.href = '/room.html'; // 매니저 버튼 클릭 시 '/rooms'으로 리다이렉트
+});
