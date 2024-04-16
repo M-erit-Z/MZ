5142d4cd315a5e0d33e1e97df3867e6f3a1443e0
k-minsik <smin58810@gmail.com>
2024-04-16 10:19:36 +0900

    feat: client, manager 페이지 동적추가

--- backend/meritz/src/main/java/com/meritz/ThymeleafController.java
+++ backend/meritz/src/main/java/com/meritz/ThymeleafController.java
@@ -1,12 +1,27 @@
 package com.meritz;
 
+import com.meritz.room.dto.CreateRoomRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ThymeleafController {
+
+    @GetMapping("/client/{roomId}")
+    public String clientPage(@PathVariable("roomId") Long roomId, Model model) {
+        model.addAttribute("roomId : ", roomId);
+        return "client";
+    }
+
+    @GetMapping("/manager/{roomId}")
+    public String managerPage(@PathVariable("roomId") Long roomId, @RequestBody CreateRoomRequest in, Model model) {
+        model.addAttribute("roomId", roomId);
+        model.addAttribute("roomRequest", in);
+        return "manager";
+    }
 
     @GetMapping("/record/{roomId}")
     public String recordPage(@PathVariable("roomId") Long roomId, Model model) {
