bb79b858280aec7c09a916a453f0f1ad5b1391ad
k-minsik <smin58810@gmail.com>
2024-04-16 13:17:53 +0900

    feat: 개인 기록 열람 기능 추가

--- backend/meritz/src/main/java/com/meritz/client/controller/ClientController.java
+++ backend/meritz/src/main/java/com/meritz/client/controller/ClientController.java
@@ -1,11 +1,45 @@
 package com.meritz.client.controller;
 
+import com.meritz.client.dto.GetClientRequest;
+import com.meritz.client.entity.Client;
+import com.meritz.client.repository.ClientRepository;
+import com.meritz.client.service.ClientService;
+import com.meritz.room.entity.Room;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ClientController {
+
+    private final ClientService clientService;
+    private final ClientRepository clientRepository;
+
+    @GetMapping("/api/clients")
+    public ResponseEntity<?> getClientId(@RequestParam("clientName") String clientName,
+                                         @RequestParam("clientPhone") String clientPhone) {
+        Optional<Client> client = clientRepository.findByClientNameAndAndClientPhone(clientName, clientPhone);
+        if (client.isPresent()) {
+            return ResponseEntity.ok(client.get().getId());
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Client Not Found.");
+        }
+    }
+
+    @GetMapping("/api/clients/{clientId}")
+    public ResponseEntity<?> getHistory(@PathVariable("clientId") Long clientId) {
+        Optional<Client> client = clientRepository.findById(clientId);
+        if (client.isPresent()) {
+            return ResponseEntity.ok(clientService.getAll(client.get()));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Client Not Found.");
+        }
+    }
+
 }
--- backend/meritz/src/main/java/com/meritz/client/service/ClientService.java
+++ backend/meritz/src/main/java/com/meritz/client/service/ClientService.java
@@ -1,11 +1,51 @@
 package com.meritz.client.service;
 
+import com.meritz.client.entity.Client;
+import com.meritz.room.dto.GetRoomsResponse;
+import com.meritz.room.entity.Room;
+import com.meritz.room.entity.RoomStatus;
+import com.meritz.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ClientService {
+
+    private final RoomRepository roomRepository;
+
+    public Map<RoomStatus, List<GetRoomsResponse>> getAll(Client client) {
+        List<Room> rooms = roomRepository.findByClient(client);
+
+        Map<RoomStatus, List<GetRoomsResponse>> out = new HashMap<>();
+        for (RoomStatus status : RoomStatus.values()) {
+            List<GetRoomsResponse> filteredRooms = rooms.stream()
+                    .filter(room -> room.getStatus() == status)
+                    .map(this::getAllResponse)
+                    .collect(Collectors.toList());
+            out.put(status, filteredRooms);
+        }
+
+        return out;
+    }
+
+    public GetRoomsResponse getAllResponse(Room room) {
+        return GetRoomsResponse.builder()
+                .roomId(room.getId())
+                .clientName(room.getClient().getClientName())
+                .location(room.getLocation())
+                .occurTime(room.getCreatedAt())
+                .managerName(room.getManager().getManagerName())
+                .status(room.getStatus())
+                .build();
+    }
+
+
 }
