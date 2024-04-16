daeb1d47c6c20dd27049f52bd00c74a16a95d7ac
k-minsik <smin58810@gmail.com>
2024-04-16 10:17:24 +0900

    fix: 메서드명 변경

--- backend/meritz/src/main/java/com/meritz/client/repository/ClientRepository.java
+++ backend/meritz/src/main/java/com/meritz/client/repository/ClientRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findByClientNameAndAndPhoneNumber(String clientName, String phoneNumber);
+    Optional<Client> findByClientNameAndAndClientPhone(String clientName, String clientPhone);
 }
