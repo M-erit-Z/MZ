25d298e73c6e1af08ac577921843a5ba47859775
k-minsik <smin58810@gmail.com>
2024-04-16 10:16:39 +0900

    feat: 현재위치 API 추가

--- backend/meritz/src/main/resources/static/js/register.js
+++ backend/meritz/src/main/resources/static/js/register.js
@@ -1,7 +1,7 @@
 document.getElementById('accidentForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const clientName = document.getElementById('clientName').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
+    const clientPhone = document.getElementById('clientPhone').value;
     const location = document.getElementById('location').value;
 
     // Fetch API를 사용하여 서버에 데이터 전송
@@ -12,7 +12,7 @@ document.getElementById('accidentForm').addEventListener('submit', function(e) {
         },
         body: JSON.stringify({
             clientName: clientName,
-            phoneNumber: phoneNumber,
+            clientPhone: clientPhone,
             location: location
         })
     })
@@ -25,29 +25,42 @@ document.getElementById('accidentForm').addEventListener('submit', function(e) {
 });
 
 document.getElementById('locateButton').addEventListener('click', function() {
-    if ('geolocation' in navigator) {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            const lat = position.coords.latitude;
-            const lng = position.coords.longitude;
-            getKakaoAddress(lat, lng);
+            var lat = position.coords.latitude,
+                lng = position.coords.longitude,
+                coord = new kakao.maps.LatLng(lat, lng),
+                geocoder = new kakao.maps.services.Geocoder();
+
+            var callback = function(result, status) {
+                if (status === kakao.maps.services.Status.OK) {
+                    var address = result[0].address.address_name;
+                    document.getElementById('location').value = address;
+                    console.log('현재 위치의 주소는 ' + address + ' 입니다.');
+                }
+            };
+
+            geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
         }, function(error) {
-            console.error('Geolocation error:', error);
+            console.error('Geolocation 실패: ', error);
+            alert('위치를 가져올 수 없습니다.');
         });
     } else {
-        alert('Geolocation API를 지원하지 않는 브라우저입니다.');
+        alert('이 브라우저에서는 지오로케이션을 사용할 수 없습니다.');
     }
 });
 
-function getKakaoAddress(lat, lng) {
-    const geocoder = new kakao.maps.services.Geocoder();
 
-    const coord = new kakao.maps.LatLng(lat, lng);
-    const callback = function(result, status) {
-        if (status === kakao.maps.services.Status.OK) {
-            console.log('그런 너의 주소는 ' + result[0].address.address_name);
-            document.getElementById('location').value = result[0].address.address_name;
-        }
-    };
-
-    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-}
+// document.getElementById('locateButton').addEventListener('click', function() {
+//     const geocoder = new kakao.maps.services.Geocoder();
+//
+//     const coord = new kakao.maps.LatLng(lat, lng);
+//     const callback = function(result, status) {
+//         if (status === kakao.maps.services.Status.OK) {
+//             console.log('그런 너의 주소는 ' + result[0].address.address_name);
+//             document.getElementById('location').value = result[0].address.address_name;
+//         }
+//     };
+//
+//     geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+// });
